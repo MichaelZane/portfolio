0b87b49207d39e6dbee5dc0518d58a28515910bb
Michael Stevens <stevensmike36@yahoo.com>
2020-07-25 19:09:27 -0500

    fixed icon

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
         stevensmike36@yahoo.com
         <br/>
         <br/>
-        {/* <a href = "stevensmike36@yahoo.com"><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a> */}
         <a href = "https://www.linkedin.com/in/michael-stevens-dev/" alt="linked in icon"><i class="fa fa-linkedin-square fa-3x"></i></a>
         <a href = "https://github.com/mzstevens69" alt="github icon"><i class="fab fa-github-square fa-3x" ></i></a>
         <a href = "https://twitter.com/Michaelzanemike" alt="twitter icon"><i class="fab fa-twitter-square fa-3x" ></i></a>
